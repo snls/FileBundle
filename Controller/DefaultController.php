0a4daf12478f72dab05a045909c203ed4c15c6e8
snls <ice_ujin@inbox.ru>
2016-08-28 20:06:49 +0300

    create file upload bundle

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,16 +2,54 @@
 
 namespace FileBundle\Controller;
 
+use FileBundle\Entity\File;
+use FileBundle\Form\FileUploadType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="file_list")
      */
     public function indexAction()
     {
-        return $this->render('FileBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getRepository("FileBundle:File");
+        $files = $em->findAll();
+
+        return $this->render('FileBundle:Default:index.html.twig', [
+            'files' => $files
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="file_upload")
+     */
+    public function createAction(Request $request)
+    {
+        $file = new File();
+        $form = $this->createForm(FileUploadType::class, $file);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $fileT = $file->getName();
+            $fileN = md5(uniqid()).'.'.$fileT->guessExtension();
+            $fileT->move(
+                $this->getParameter('file_directories'),
+                $fileN
+            );
+            $file->setName($fileN);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($file);
+            $em->flush();
+
+            return $this->redirectToRoute('file_list');
+        }
+
+        return $this->render('FileBundle:Default:create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
